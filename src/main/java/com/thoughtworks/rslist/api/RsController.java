85aa9e40794dba9709a08e68c3e1f10febe9e6c7
Anson <1072476988@qq.com>
2020-09-15 18:09:48 +0800

    [lingzhen] finish requirement 1/2/3.

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,38 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.rslist.domain.RsEvent;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList = initRsEventList();
+
+  private List<RsEvent> initRsEventList() {
+    List<RsEvent> rsEventList = new ArrayList<>();
+    rsEventList.add(new RsEvent("第一条事件","无标签"));
+    rsEventList.add(new RsEvent("第二条事件","无标签"));
+    rsEventList.add(new RsEvent("第三条事件","无标签"));
+    return rsEventList;
+  }
+
+  @GetMapping("rs/{index}")
+  public RsEvent getOneRsEvent(@PathVariable int index){
+    return rsList.get(index-1);
+  }
+
+  @GetMapping("rs/list")
+  public List<RsEvent> getRsEventBetween(@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end){
+    if (start == null || end == null){
+      return rsList;
+    }
+    return rsList.subList(start-1,end);
+  }
+
+  @PostMapping("/rs/event")
+  public void addRsEvent(@RequestBody RsEvent rsEvent){
+    rsList.add(rsEvent);
+  }
 }
